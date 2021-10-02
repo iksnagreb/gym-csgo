ee45d827d34191c3f4963a646bb2da5673ad3d7b
Christoph Berganski <christoph.berganski@gmail.com>
2021-10-03 01:31:17 +0200

    Add package metadata to setup.py and add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,6 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+# Read long description from README
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
 
 # Setup the python package with dependencies
-setup(name='gym_csgo', version='0.0.1', install_requires=[
-    'gym', 'mss', 'numpy', 'opencv-python', 'flask', 'pynput', 'pygame'
-])
+setup(
+    # Package name and version and contained packages
+    name='gym_csgo', version='0.0.1', packages=find_packages(),
+    # Package metadata
+    description="Counter-Strike: Global Offensive environment for OpenAI Gym "
+                "on Linux",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iksnagreb/gym-csgo",
+    author="Christoph Berganski",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux"
+    ],
+    # Package requirements
+    install_requires=[
+        'gym', 'mss', 'numpy', 'opencv-python', 'flask', 'pynput', 'pygame'
+    ],
+    # Add non-code files to package
+    include_package_data=True,
+)
